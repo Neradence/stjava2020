cecb35c56a690188d0eeef1b0db6e4f6f944a766
Neradence <neradence@gmail.com>
2020-05-19 10:07:49 +0300

    Add the class Point and a function and a method for it

--- sandbox/src/main/java/Hello.java
+++ sandbox/src/main/java/Hello.java
@@ -1,5 +1,21 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+
+        Point One = new Point(1,2);
+        Point Two = new Point(4, -7);
+
+        Point Three = new Point(-6.2, 8);
+        Point Four = new Point(0, 12);
+
+        System.out.println(distance(One, Two));
+        System.out.println(distance(Three, Four));
+
+        System.out.println(One.anotherDistance(Three));
+        System.out.println(Two.anotherDistance(Four));
+    }
+
+    public static double distance(Point p1, Point p2) {
+
+        return Math.sqrt((Math.pow((p2.x - p1.x),2)) + (Math.pow((p2.y - p1.y),2)));
     }
 }
