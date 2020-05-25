f32111205d4931479a99660172263ec0aeb42027
Neradence <neradence@gmail.com>
2020-05-25 15:14:14 +0300

    Add new module Adressbook and tests for it

---
+++ adressbook-web-test/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,31 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String contactname;
+    private final String contactsurname;
+    private final String contactphone;
+    private final String contactmail;
+
+    public ContactData(String contactname, String contactsurname, String contactphone, String contactmail) {
+        this.contactname = contactname;
+        this.contactsurname = contactsurname;
+        this.contactphone = contactphone;
+        this.contactmail = contactmail;
+    }
+
+    public String getContactname() {
+        return contactname;
+    }
+
+    public String getContactsurname() {
+        return contactsurname;
+    }
+
+    public String getContactphone() {
+        return contactphone;
+    }
+
+    public String getContactmail() {
+        return contactmail;
+    }
+}
