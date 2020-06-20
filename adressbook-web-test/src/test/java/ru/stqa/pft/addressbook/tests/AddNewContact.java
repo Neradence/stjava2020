f3594c8029b819ac614fc57a49d81eb433f8fb08
neradence <neradence@gmail.com>
2020-06-20 13:22:32 +0300

    Add dataproviders for contacts and groups creation

--- adressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/AddNewContact.java
+++ adressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/AddNewContact.java
@@ -1,23 +1,34 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.models.ContactData;
 import ru.stqa.pft.addressbook.models.Contacts;
+import ru.stqa.pft.addressbook.models.GroupData;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 public class AddNewContact extends TestBase {
 
-  @Test
-  public void testAddNewContact() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validContacts() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new ContactData().withContactname("name1").withContactsurname("surname1").withContactaddress("address1").withEmail1("email1").withContactphone("1234")});
+    list.add(new Object[] {new ContactData().withContactname("name2").withContactsurname("surname2").withContactaddress("address2").withEmail1("email2").withContactphone("1234")});
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validContacts")
+  public void testAddNewContact(ContactData contact) throws Exception {
     app.goTo().allContactTab();
     Contacts before = app.contact().allContact();
     app.goTo().addContactTab();
-    File photo = new File("src/test/resources/ava.jpg");
-    ContactData contact = new ContactData().withContactname("Anna").withContactsurname("Green").withContactphone("1234").withPhoto(photo);
     app.contact().fillContactForm(contact);
     app.contact().saveNewContactForm();
     app.goTo().allContactTab();
--- adressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/AddNewGroup.java
+++ adressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/AddNewGroup.java
@@ -1,20 +1,33 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.models.GroupData;
 import ru.stqa.pft.addressbook.models.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class AddNewGroup extends TestBase {
 
-  @Test
-  public void testAddNewGroup() throws Exception {
+  @DataProvider
+  public Iterator<Object[]> validGroups() {
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new GroupData().withGroupname("test1").withGroupheader("header1").withGroupfooter("footer1")});
+    list.add(new Object[] {new GroupData().withGroupname("test2").withGroupheader("header2").withGroupfooter("footer2")});
+    list.add(new Object[] {new GroupData().withGroupname("test3").withGroupheader("header3").withGroupfooter("footer3")});
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validGroups")
+  public void testAddNewGroup(GroupData group) throws Exception {
     app.goTo().groupTab();
     Groups before = app.group().allGroup();
     app.group().initGroupCreation();
-    GroupData group = new GroupData().withGroupname("Sport");
     app.group().fillGroupForm(group);
     app.group().saveFilledGroupForm();
     app.goTo().groupTab();
